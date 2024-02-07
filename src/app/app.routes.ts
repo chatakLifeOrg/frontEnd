0952f1c2c9be8419e83fcfdb9978f74f5cdd76ea
aman <amanlamba817@gmail.com>
2024-02-07 10:48:03 +0530

    add the new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductPageComponent } from './components/product-page/product-page.component';
+import { CategoryComponent } from './components/category/category.component';
 
 export const routes: Routes = [{
   path: '',
@@ -14,15 +15,26 @@ export const routes: Routes = [{
 {
   path: 'homePage',
   data: { breadcrumb: 'Home' },
+  title: 'HomePage',
   children: [
     {
-      path: 'product',
-      component: ProductPageComponent,
+      path: 'category',
+      component: CategoryComponent,
       pathMatch: 'prefix',
-      title: '',
-      data: { breadcrumb: 'Product' },
+      title: 'Category',
+    },{
+      path:'category',
+      data: { breadcrumb:'Category' },
+      children: [
+        {
+          path: 'product',
+          component: ProductPageComponent,
+          pathMatch: 'prefix',
+          title: 'Product',
+          data: { breadcrumb: 'Product' },
+        }
+      ]
     }
   ],
-  title: 'HomePage | Product'
-}
+},
 ];
